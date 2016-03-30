0304bda046ce14617c73fe69010e396797c19e16
Nik <narmstrong@universeprojects.com>
2016-03-29 23:28:16 -0400

    Got it working on another PC. Didn't do much here.

---
+++ Initium-Odp/src/com/universeprojects/miniup/server/commands/ServletCommand.java
@@ -0,0 +1,14 @@
+package com.universeprojects.miniup.server.commands;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class ServletCommand extends HttpServlet 
+{
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException 
+	{
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
--- Initium-Odp/src/com/universeprojects/miniup/server/commands/TransactionalCommand.java
+++ Initium-Odp/src/com/universeprojects/miniup/server/commands/TransactionalCommand.java
@@ -1,6 +1,17 @@
 package com.universeprojects.miniup.server.commands;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.universeprojects.cacheddatastore.CachedDatastoreService;
+
 public abstract class TransactionalCommand extends Command
 {
+
+	public TransactionalCommand(CachedDatastoreService ds,
+			HttpServletRequest request, HttpServletResponse response) {
+		super(ds, request, response);
+		// TODO Auto-generated constructor stub
+	}
 	
 }
