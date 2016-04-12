22df3d2277591350954c8590fbddde64d2c943e5
A Box of Foxes <virtualfox@outlook.com>
2016-04-12 23:00:51 +0100

    Move reloadPagePopupIframe functionality into reloadPagePopup

--- Initium-Odp/war/odp/javascript/script.js
+++ Initium-Odp/war/odp/javascript/script.js
@@ -964,8 +964,8 @@ function pagePopupIframe(url)
 	currentPopupStackIndex++;
 	var pagePopupId = "page-popup"+currentPopupStackIndex;
 	
-	$("#page-popup-root").append("<div id='"+pagePopupId+"'><iframe id='"+pagePopupId+"-content' src='"+url+"' class='page-popup' width='95%' height='90'><img id='banner-loading-icon' src='javascript/images/wait.gif' border=0/></iframe><div class='page-popup-glass'></div><a class='page-popup-Reload' onclick=reloadPagePopupIframe('"+pagePopupId+"-content'); style='font-family:Lucida Sans'>&#8635;</a><a class='page-popup-X' onclick='closePagePopup()'>X</a></div>");
-	
+	$("#page-popup-root").append("<div id='"+pagePopupId+"'><iframe id='"+pagePopupId+"-content' src='"+url+"' class='page-popup' width='95%' height='90'><img id='banner-loading-icon' src='javascript/images/wait.gif' border=0/></iframe><div class='page-popup-glass'></div><a class='page-popup-Reload' onclick='reloadPagePopup()' style='font-family:Lucida Sans'>&#8635;</a><a class='page-popup-X' onclick='closePagePopup()'>X</a></div>");
+
     if (currentPopupStackIndex==1)
     {
 	    $(document).bind("keydown",function(e) 
@@ -979,15 +979,6 @@ function pagePopupIframe(url)
 
     if (currentPopupStackIndex>1)
     	$("#page-popup"+(currentPopupStackIndex-1)).hide();
-}
-
-function reloadPagePopupIframe(id)
-{
-	var iframe = document.getElementById(id);
-	if (iframe != null)
-	{
-		iframe.src = iframe.src;
-	}
 }
 
 function closePagePopup()
@@ -1020,13 +1011,21 @@ function reloadPagePopup(quietly)
 		return;
 	
 	var pagePopupId = "page-popup"+currentPopupStackIndex;
+	var content = $("#"+pagePopupId+"-content");
 	
-	var url = $("#"+pagePopupId+"-content").attr("src");
-	if (quietly)
-	{}
-	else
-		$("#"+pagePopupId).html("<div id='"+pagePopupId+"-content' src='"+url+"' class='page-popup'><img id='banner-loading-icon' src='javascript/images/wait.gif' border=0/></div><div class='page-popup-glass'></div><a class='page-popup-Reload' onclick='reloadPagePopup()' style='font-family:Lucida Sans'>&#8635;</a><a class='page-popup-X' onclick='closePagePopup()'>X</a>");
-	$("#"+pagePopupId+"-content").load(url);
+	var url = content.attr("src");
+	if (content.is("iframe"))
+	{
+		if (quietly)
+			$("#"+pagePopupId).html("<iframe id='"+pagePopupId+"-content' src='"+url+"' class='page-popup' width='95%' height='90%'><img id='banner-loading-icon' src='javascript/images/wait.gif' border=0/></iframe><div class='page-popup-glass'></div><a class='page-popup-Reload' onclick='reloadPagePopup()' style='font-family:Lucida Sans'>&#8635;</a><a class='page-popup-X' onclick='closePagePopup()'>X</a>");
+		content.attr('src', url);
+	}
+	else 
+	{
+		if (quietly)
+			$("#"+pagePopupId).html("<div id='"+pagePopupId+"-content' src='"+url+"' class='page-popup'><img id='banner-loading-icon' src='javascript/images/wait.gif' border=0/></div><div class='page-popup-glass'></div><a class='page-popup-Reload' onclick='reloadPagePopup()' style='font-family:Lucida Sans'>&#8635;</a><a class='page-popup-X' onclick='closePagePopup()'>X</a>");
+		content.load(url);
+	}
 }
 
 function moveItem(event, itemId, newContainerKind, newContainerId)
