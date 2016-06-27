5b7bff8603518761df56a896008e61f874208042
Davos-Onomous <adam.onomous@gmail.com>
2016-06-27 15:07:46 +0000

    Add files via upload

---
+++ Initium-Odp/src/com/universeprojects/miniup/server/commands/CommandStoreDeleteAllItems.java
@@ -0,0 +1,52 @@
+package com.universeprojects.miniup.server.commands;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.universeprojects.cacheddatastore.CachedDatastoreService;
+import com.universeprojects.cacheddatastore.CachedEntity;
+import com.universeprojects.miniup.server.ODPDBAccess;
+import com.universeprojects.miniup.server.commands.framework.Command;
+import com.universeprojects.miniup.server.commands.framework.UserErrorMessage;
+/** 
+ * 
+ * Deletes all items from store interface.
+ * 
+ */
+
+public class CommandStoreDeleteAllItems extends Command {
+	
+	public CommandStoreDeleteAllItems(HttpServletRequest request, HttpServletResponse response)
+	{
+		super(request, response);
+	}
+	
+	@Override
+	public void run(Map<String, String> parameters) throws UserErrorMessage 
+	{
+		ODPDBAccess db = getDB();
+		CachedDatastoreService ds = getDS();
+		
+		Key characterKey = KeyFactory.createKey("characterKey", 1471230485);
+				
+		List<CachedEntity> saleItems = db.getSaleItemsFor(characterKey);
+		
+		for(CachedEntity item:saleItems)
+		{
+			if (item!=null)
+			{
+				if (characterKey.equals(item.getProperty("characterKey"))==false)
+					throw new IllegalArgumentException("The SellItem this user is trying to delete does not belong to his character.");
+				
+				ds.delete(item);
+			}
+		}
+		setJavascriptResponse(JavascriptResponse.ReloadPagePopup);
+	}
+	
+}
---
+++ Initium-Odp/src/com/universeprojects/miniup/server/commands/CommandStoreSellItem.java
@@ -0,0 +1,60 @@
+package com.universeprojects.miniup.server.commands;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.universeprojects.cacheddatastore.CachedDatastoreService;
+import com.universeprojects.cacheddatastore.CachedEntity;
+import com.universeprojects.miniup.server.ODPDBAccess;
+import com.universeprojects.miniup.server.JspSnippets;
+import com.universeprojects.miniup.server.commands.framework.Command;
+import com.universeprojects.miniup.server.commands.framework.UserErrorMessage;
+import com.universeprojects.miniup.server.commands.framework.Command.JavascriptResponse;
+/** 
+ * 
+ * Sell an Item!
+ * 
+ */
+
+public class CommandStoreSellItem<character> extends Command {
+	
+	public CommandStoreSellItem(HttpServletRequest request, HttpServletResponse response)
+	{
+		super(request, response);
+	}
+	
+	@Override
+public void run(Map<String,String> parameters) throws UserErrorMessage {
+		
+		if (amount<0)
+			throw new UserErrorMessage("You cannot sell an item for less than 0 gold.");
+		
+		ODPDBAccess db = getDB();
+		CachedDatastoreService ds = getDS();
+		CachedEntity character = db.getCurrentCharacter(request);
+		CachedEntity item = db.getEntity("Item", tryParseId(parameters, "itemId")); 
+		
+		Key itemKey = KeyFactory.createKey("Item", itemId);
+		Key characterKey = KeyFactory.createKey("characterKey", 123451234);
+		
+		if (db.checkCharacterHasItemEquipped(character, itemKey))
+			throw new UserErrorMessage("Unable to sell this item, you currently have it equipped.");
+		
+		if (item==null)
+			throw new UserErrorMessage("Item doesn't exist.");
+		
+		if (item.getProperty("containerKey").equals(character.getKey())==false)
+			throw new UserErrorMessage("You do not have this item. You cannot sell an item that is not in your inventory.");
+		
+		if (db.checkItemBeingSoldAlready(character.getKey(), itemKey))
+			throw new UserErrorMessage("You are already selling that item. If you want to change the price, remove the existing entry first.");
+		
+		newSaleItem(db, character, item, amount);
+	}
+	
+}
