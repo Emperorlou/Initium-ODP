317d4aa14246b8c87dc0e450cdffc7d1a7e6901b
Emperorlou <nikolasarmstrong@gmail.com>
2016-08-07 05:09:48 -0400

    Added the territory view as a method in HtmlComponents.

--- Initium-Odp/src/com/universeprojects/miniup/server/HtmlComponents.java
+++ Initium-Odp/src/com/universeprojects/miniup/server/HtmlComponents.java
@@ -10,6 +10,80 @@ import com.universeprojects.cacheddatastore.CachedEntity;
 
 public class HtmlComponents {
 
+	public static String generateTerritoryView(CachedEntity character, CachedEntity territoryOwnerGroup, CachedEntity territory)
+	{
+		if (territory==null) return "";
+		
+		String groupName = "<No owner>";
+		if (territoryOwnerGroup!=null)
+			groupName = (String)territoryOwnerGroup.getProperty("name");
+		String groupStatus = (String)character.getProperty("groupStatus");
+		boolean isOwningGroupMember = false;
+		if (GameUtils.equals(territoryOwnerGroup.getKey(),character.getProperty("groupKey")) && 
+				("Member".equals(groupStatus) || "Admin".equals(groupStatus)))
+			isOwningGroupMember = true;
+		boolean isOwningGroupAdmin = false;
+		if ("Admin".equals(groupStatus) && isOwningGroupMember)
+			isOwningGroupAdmin = true;
+		String characterStatus = (String)character.getProperty("status");
+		
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append("<div class='boldbox' id='territoryView'>");
+		sb.append("<h4>Territory</h4>");
+		sb.append("<h5>Owning Group: "+groupName+"</h5>");
+		sb.append("<div class='main-item-controls'>");
+		sb.append("<a onclick='doTerritoryClaim(event)'>Claim Territory</a>");
+		sb.append("<a onclick='doTerritoryRetreat(event)'>Retreat from Territory</a>");
+		sb.append("</div>");
+		if (isOwningGroupAdmin)
+		{
+			sb.append("<h5>Admin Controls</h5>");
+			sb.append("<div class='main-item-controls'>");
+			sb.append("<a onclick='doTerritoryVacate(event)'>Vacate and Surrender Territory</a>");
+			sb.append("</div>");
+		}
+		sb.append("<br>");
+		sb.append("<br>");
+		sb.append("<div style='text-align:center'>");
+		sb.append("<div class='smallbox'>");
+		sb.append("1st Line");
+//		sb.append("<p><span class='hint' rel='#engagedDefenders'>${defenderEngaged1Count}</span>/<span class='hint' rel='#defenderCount'>${defender1Count}</span></p>");
+		if ("Defending1".equals(characterStatus))
+			sb.append("<p class='hint' rel='#joinedDefenderGroup'>Joined</p>");
+		else
+			sb.append("<p><a onclick='doTerritorySetDefense(event, \"Defending1\")'>Join</a></p>");
+		sb.append("</div>");
+		sb.append("<div class='smallbox'>");
+		sb.append("2nd Line");
+//		sb.append("<p><span class='hint' rel='#engagedDefenders'>${defenderEngaged2Count}</span>/<span class='hint' rel='#defenderCount'>${defender2Count}</span></p>");
+		if ("Defending2".equals(characterStatus))
+			sb.append("<p class='hint' rel='#joinedDefenderGroup'>Joined</p>");
+		else
+			sb.append("<p><a onclick='doTerritorySetDefense(event, \"Defending2\")'>Join</a></p>");
+		sb.append("</div>");
+		sb.append("<div class='smallbox'>");
+		sb.append("3rd Line");
+//		sb.append("<p><span class='hint' rel='#engagedDefenders'>${defenderEngaged3Count}</span>/<span class='hint' rel='#defenderCount'>${defender3Count}</span></p>");
+		if ("Defending3".equals(characterStatus))
+			sb.append("<p class='hint' rel='#joinedDefenderGroup'>Joined</p>");
+		else
+			sb.append("<p><a onclick='doTerritorySetDefense(event, \"Defending3\")'>Join</a></p>");
+		sb.append("</div>");
+		sb.append("<div class='smallbox'>");
+		sb.append("Not Defending");
+//		sb.append("<p>${notDefendingCount}</p>");
+		if (characterStatus==null || "Normal".equals(characterStatus))
+			sb.append("<p class='hint' rel='#joinedDefenderGroup'>Joined</p>");
+		else
+			sb.append("<p><a onclick='doTerritorySetDefense(event, \"Normal\")'>Join</a></p>");
+		sb.append("</div>");
+		sb.append("</div>");
+		sb.append("</div>");
+		
+		return sb.toString();
+	}
+	
 	public static String generateInvItemHtml(CachedEntity item) {
 		
 		if (item==null)
