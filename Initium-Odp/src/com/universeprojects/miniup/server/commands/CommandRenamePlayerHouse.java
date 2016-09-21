1b2140497ce838b4308587ed21a4eb8c395de60d
jDyn90 <josephbae@gmail.com>
2016-09-21 11:47:03 -0700

    Update CommandRenamePlayerHouse.java

--- Initium-Odp/src/com/universeprojects/miniup/server/commands/CommandRenamePlayerHouse.java
+++ Initium-Odp/src/com/universeprojects/miniup/server/commands/CommandRenamePlayerHouse.java
@@ -1,4 +1,23 @@
-public void run(Map<String, String> parameters) throws UserErrorMessage {
+package com.universeprojects.miniup.server.commands;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.universeprojects.cacheddatastore.CachedDatastoreService;
+import com.universeprojects.cacheddatastore.CachedEntity;
+import com.universeprojects.miniup.server.ODPDBAccess;
+import com.universeprojects.miniup.server.commands.framework.Command;
+import com.universeprojects.miniup.server.commands.framework.UserErrorMessage;
+
+public class CommandRenamePlayerHouse extends Command {
+
+	public CommandRenamePlayerHouse(ODPDBAccess db, HttpServletRequest request, HttpServletResponse response) {
+		super(db, request, response);
+	}
+	
+	public void run(Map<String, String> parameters) throws UserErrorMessage {
 		ODPDBAccess db = getDB();
 		CachedDatastoreService ds = getDS();
 		CachedEntity character = db.getCurrentCharacter();
@@ -23,3 +42,4 @@ public void run(Map<String, String> parameters) throws UserErrorMessage {
 			ds.put(location);
 		}
 	}
+}
