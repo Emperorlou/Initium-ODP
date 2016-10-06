2339cccc3f9c74669c483323600b39bdae11da79
Sergey Potupchik <spfiredrake@gmail.com>
2016-10-06 15:01:23 -0400

    Comments

--- Initium-Odp/src/com/universeprojects/miniup/server/scripting/jsaccessors/DBAccessor.java
+++ Initium-Odp/src/com/universeprojects/miniup/server/scripting/jsaccessors/DBAccessor.java
@@ -55,7 +55,7 @@ public class DBAccessor {
 	
 	public boolean executeScript(ScriptEvent currentEvent, String scriptName, Object... entities)
 	{
-		List<CachedEntity> scripts = db.getFilteredList("Script", "internalName", scriptName);
+		List<CachedEntity> scripts = db.getFilteredList("Script", "name", scriptName);
 		if(scripts.size() > 1)
 			throw new RuntimeException("Duplicate scripts detected: " + scriptName);
 		if(scripts.size() == 0)
@@ -69,11 +69,12 @@ public class DBAccessor {
 		boolean executed = service.executeScript(event, script, (EntityWrapper)null);
 		if(executed)
 		{
+			// Same thing, pass off the saving/deleting of entities to the original context. 
 			for(EntityWrapper save:event.saveEntities)
 				currentEvent.saveEntity(save);
 			for(EntityWrapper delete:event.deleteEntities)
 				currentEvent.deleteEntity(delete);
-			currentEvent.haltExecution = event.haltExecution;
+			currentEvent.haltExecution |= event.haltExecution;
 			currentEvent.errorText += event.errorText;
 			currentEvent.descriptionText += event.descriptionText;
 		}
