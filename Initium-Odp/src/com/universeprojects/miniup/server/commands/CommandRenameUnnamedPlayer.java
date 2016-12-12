51885403b104e612fa263a1964a8c0f5961aa8be
John Amstadt <john.amstadt@erdosmiller.com>
2016-12-12 06:53:17 -0600

    Implemented changes and suggestions from pull request (#217) review.

--- Initium-Odp/src/com/universeprojects/miniup/server/commands/CommandRenameUnnamedPlayer.java
+++ Initium-Odp/src/com/universeprojects/miniup/server/commands/CommandRenameUnnamedPlayer.java
@@ -14,38 +14,52 @@ import com.universeprojects.miniup.server.commands.framework.UserErrorMessage;
 import com.universeprojects.miniup.server.commands.framework.Command.JavascriptResponse;
 import com.universeprojects.miniup.server.services.PropertiesService;
 
-//ODPDBAccess.isCharacterNameOk()
+/**
+ * Renname Unnamed character command.
+ * 
+ * @author RevMuun
+ * 
+ */
 
 public class CommandRenameUnnamedPlayer extends Command {
 	
-	//private HttpServletRequest theRequest;
-
+	/**
+	 * Command provides rennaming capabilities for unnamed characters.
+	 * @param db
+	 * 			  ODP Database
+	 * @param request
+	 *            Server request
+	 * @param response
+	 *            Server response
+	 */
 	public CommandRenameUnnamedPlayer(ODPDBAccess db, HttpServletRequest request, HttpServletResponse response) {
-		super(db, request, response);
-		//theRequest = request;
+		super(db, request, response);		
 	}
 
 	@Override
 	public void run(Map<String, String> parameters) throws UserErrorMessage {
-		// TODO Auto-generated method stub
+		
+		//get the ODP database object.
 		ODPDBAccess db = getDB();
+		//grab the CachedDatastoreService (seems to be a singleton)
 		CachedDatastoreService ds = getDS();
+		//get the current character
 		CachedEntity character = db.getCurrentCharacter();
 		
+		//grab the current character name as well as the one provided by the user.
 		String charName = (String) character.getProperty("name");
 		String newName = parameters.get("newName");
 		
-		if(charName == "Unnamed"){
+		//this command is intended only for characters with the name "Unnamed". Case sensitive.
+		if(charName.equals("Unnamed")){
 			//looking at ODPDBAccess.isCharacterNameOk(), it appears to do all the validation. Other than checking
 			//if the character is named "Unnamed" no extra validation will be done here.
-			try{
-				if(db.isCharacterNameOk(this.request, charName)){
-					
-				}
-			}
-			catch(UserErrorMessage uem){
-				throw uem;
-			}
+			if(db.isCharacterNameOk(this.request, charName)){
+				
+					//everything looks good. Update the CachedEntity and put it in the database.
+					character.setProperty("name", newName);
+					ds.put(character);
+			}			
 		}
 		else{
 			throw new UserErrorMessage("Only \"Unnamed\" characters can be renamed");
