005833be6667a88cda43fc102c3f3aba16fd5c43
spfiredrake <spfiredrake@gmail.com>
2016-12-14 12:34:11 -0500

    Additional merge request cancel fixes.

--- Initium-Odp/src/com/universeprojects/miniup/server/commands/CommandGroupMergeCancelRequest.java
+++ Initium-Odp/src/com/universeprojects/miniup/server/commands/CommandGroupMergeCancelRequest.java
@@ -30,24 +30,36 @@ public class CommandGroupMergeCancelRequest extends Command {
 		if(character.getProperty("groupKey") == null)
 			throw new UserErrorMessage("Character does not belong to a group!");
 		
+		Long groupID = parameters.containsKey("groupId") ? tryParseId(parameters, "groupId") : null;
+		if(groupID == null) throw new RuntimeException("Command missing parameter groupId");
+		
+		CachedEntity group = db.getEntity("Group", groupID);
+		boolean viewingOwnGroup = false;
+		try{
+			viewingOwnGroup = parameters.containsKey("ownGroup") && Boolean.parseBoolean(parameters.get("ownGroup"));
+		} catch(Exception x){}
+		
 		// Group refers to the group we will be merging with. GroupService handles security 
 		// permissions, determining whether the character is an admin of his group.
 		GroupService service = new GroupService(db, character);
-		CachedEntity group = service.getCharacterGroup();
+		CachedEntity charGroup = service.getCharacterGroup();
 		if(!service.cancelMergeRequest())
 		{
 			if(service.characterHasGroup() == false)
 				throw new UserErrorMessage("You do not currently belong to a group");
 			if(service.isCharacterGroupAdmin() == false)
 				throw new UserErrorMessage("You are not a group admin");
-			if(service.getMergeRequestGroupKeyFor(group) == null)
+			if(service.getMergeRequestGroupKeyFor(charGroup) == null)
 				throw new UserErrorMessage("Your group does not have a pending merge request with any group");
 			throw new UserErrorMessage("Unexpected error cancelling merge requests! Please contact a dev.");
 		}
 		
 		// property was updated by the service. Save group and update the HTML. 
-		ds.put(group);
-		updateHtml("#groupMergeRequest", "<a id='groupMergeRequest' onclick='groupMergeSubmitRequest(event, "+group.getId()+")' title='Clicking this will submit a request to merge with the current group.'>Request Merge With Group</a>");
+		ds.put(charGroup);
+		if(viewingOwnGroup)
+			updateHtml("#groupMergeRequest", "Pending merge cancelled.");
+		else
+			updateHtml("#groupMergeRequest", "<a id='groupMergeRequest' onclick='groupMergeSubmitRequest(event, "+group.getId()+")' title='Clicking this will submit a request to merge with the current group.'>Request Merge With Group</a>");
 	}
 
 }
