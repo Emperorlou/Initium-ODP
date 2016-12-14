aef03c90403013ba49e75e551c9a1b6585fbfdf8
John Amstadt <john.amstadt@erdosmiller.com>
2016-12-14 08:46:45 -0600

    First implementation of SwapStorageContainer. Ready for code review.

--- Initium-Odp/src/com/universeprojects/miniup/server/commands/CommandItemsSwapStorageContainers.java
+++ Initium-Odp/src/com/universeprojects/miniup/server/commands/CommandItemsSwapStorageContainers.java
@@ -1,41 +1,83 @@
 package com.universeprojects.miniup.server.commands;
 
+import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.universeprojects.cacheddatastore.CachedDatastoreService;
+import com.universeprojects.cacheddatastore.CachedEntity;
+import com.universeprojects.miniup.server.GameUtils;
 import com.universeprojects.miniup.server.ODPDBAccess;
-import com.universeprojects.miniup.server.commands.framework.Command;
 import com.universeprojects.miniup.server.commands.framework.UserErrorMessage;
+import com.universeprojects.miniup.server.services.ContainerService;
 
 /*
- * 
-[8:32]  might be best if we use multiple selection for this
-[8:32]  there are a few commands that use a multiple-selection abstract command as a base
-[8:33]  we would basically just have 2 chests selected, one needs to be empty I guess (for lack of a better way)
-[8:34]  clicking the "swap" link would cause all the items to go from one to the other
-[8:34]  specifically, the chest with items would have all items move to the chest without items
-[8:34]  I guess any gold in the chest would move too
-[8:34]  if there is any
- * 
-initiumdev [8:36 AM]  
-if we wanted to get fancy later, we could maybe have a little popup that asks which direction the swap should go
-
-papa_marsh [8:36 AM]  
-also need to check to make sure the new chest has enough maxSpace and maxWeight to fit everything
+ * 	Author: RevMuun
  */
 
-public class CommandItemsSwapStorageContainers extends Command {
+public class CommandItemsSwapStorageContainers extends CommandItemsBase {
 
 	public CommandItemsSwapStorageContainers (ODPDBAccess db, HttpServletRequest request, HttpServletResponse response){
 		super(db,request,response);
 	}
 	
 	@Override
-	public void run(Map<String, String> parameters) throws UserErrorMessage {
-		// TODO Auto-generated method stub
-
+	protected void processBatchItems(Map<String, String> parameters, ODPDBAccess db, 
+			CachedDatastoreService ds, CachedEntity character, List<CachedEntity> batchItems) throws UserErrorMessage{
+		
+			//we're swapping 2 containers here. If there's more than 2 items in the list, something went wrong.
+			if(batchItems.size() != 2)
+				throw new UserErrorMessage("Error in item selection. Only 2 containers can be involved in a swap.");
+				
+			ContainerService cs = new ContainerService(db);
+				
+			//make sure both items are containers and character has access to them.
+			for(CachedEntity item : batchItems){					
+				if(!GameUtils.isStorageItem(item))
+					throw new UserErrorMessage("You must select two storage items to swap.");
+				if(!cs.checkContainerAccessAllowed(character, item))
+					throw new UserErrorMessage("Character does not have access to container.");
+			}
+			
+			//check that at least one container is empty.
+			if(db.getItemContentsFor(batchItems.get(0).getKey()).size() != 0 && 
+					db.getItemContentsFor(batchItems.get(1).getKey()).size() != 0){
+				throw new UserErrorMessage("At least one storage item must be empty in order to swap contents.");
+			}				
+			
+			//determine which container is empty. Since we checked that at least one container is empty,
+			//we can assume that if index 1 has equipment, then index 0 is empty.
+			int emptyContainerIndex = 0;
+			int fullContainerIndex = 1;
+			if(db.getItemContentsFor(batchItems.get(1).getKey()).size() == 0){
+				emptyContainerIndex = 1;
+				fullContainerIndex = 0;
+			}
+			
+			Long spaceReq = db.getItemCarryingSpace(batchItems.get(fullContainerIndex));
+			Long weightReq = db.getItemCarryingWeight(batchItems.get(fullContainerIndex));
+			Long spaceAvail = (Long) batchItems.get(emptyContainerIndex).getProperty("maxSpace");
+			Long weightAvail = (Long) batchItems.get(emptyContainerIndex).getProperty("maxWeight");
+			
+			//check that we have room in the new container to fit everything.
+			if(spaceAvail < spaceReq)
+				throw new UserErrorMessage("There is not enough space available in the new container.");
+			if(weightAvail < weightReq)
+				throw new UserErrorMessage("There is not enough weight available in the new container.");
+			
+			//actually move the content now and save to database.
+			ds.beginBulkWriteMode();
+			for(CachedEntity item : db.getItemContentsFor(batchItems.get(fullContainerIndex).getKey())){
+				
+				item.setProperty("containerKey",batchItems.get(emptyContainerIndex));
+				item.setProperty("moveTimestamp", new Date());
+				
+				ds.put(item);				
+			}			
+			ds.commitBulkWrite();
 	}
 
 }
