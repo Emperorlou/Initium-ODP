98048fa3241c07a99697e1c60eab1ed9c42d3a98
John Amstadt <john.amstadt@erdosmiller.com>
2016-12-16 21:04:52 -0600

    Added getOnline and getOffline functions to friends service.

--- Initium-Odp/src/com/universeprojects/miniup/server/services/FriendService.java
+++ Initium-Odp/src/com/universeprojects/miniup/server/services/FriendService.java
@@ -1,6 +1,7 @@
 package com.universeprojects.miniup.server.services;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import com.google.appengine.api.datastore.Key;
@@ -16,11 +17,13 @@ import com.universeprojects.miniup.server.ODPDBAccess;
 public class FriendService extends Service {
 	
 	private CachedEntity user;
+	private List<CachedEntity> friendsOfUser;
 
 	public FriendService(ODPDBAccess db, CachedEntity user) {
 		super(db);
 
-		this.user = user;		
+		this.user = user;
+		this.friendsOfUser = _getFriendsOfUser();
 	}
 	
 	/**
@@ -29,43 +32,75 @@ public class FriendService extends Service {
 	 */
 	public List<CachedEntity> getFriendsOfUser(){
 		
-		//get a list of Friend entities who's userKey is equal to the user.
-		List<CachedEntity> friends = db.getFilteredList("Friend","userKey", this.user.getKey());
+		return friendsOfUser;	
 		
-		//From the list of friends, get all of their user keys.
-		List<Key> friendUserKeys = new ArrayList<Key>();
-		for(CachedEntity friend : friends){
-			
-			Key friendKey = (Key) friend.getProperty("friendUserKey");
-			
-			friendUserKeys.add(friendKey);
-		}
-		
-		//return a list of User entities from the friend's user keys.
-		return db.getEntities(friendUserKeys);	
 	}
 	
 	/**
-	 * Returns a list of User entities that are friends of the user.
-	 * @param user CachedEntity representing a User.
-	 * @return List of CachedEntity representing Users that are friends of User.
+	 * Returns a list of friends (User entities) that have been active in the past 10 minutes and aren't hiding
+	 * online activity.
+	 * @return List of CachedEntity representing Users that are online.
 	 */
-	public List<CachedEntity> getFriendsOfUser(CachedEntity user){
+	public List<CachedEntity> getOnlineFriends(){
 		
+		List<CachedEntity> onlineFriends = new ArrayList<CachedEntity>();
+		
+		for(CachedEntity friend : friendsOfUser){
+			Date lastMovement = (Date) friend.getProperty("locationEntryDatetime");
+			
+			boolean online = ((new Date().getTime() - lastMovement.getTime()) >= (10*60*1000));
+			Boolean hideActivity = (Boolean) friend.getProperty("hideUserActivity");
+			
+			//if the entry datetime has occured within the past 10 minutes and user activity isn't hidden
+			if(lastMovement != null && online && !hideActivity.booleanValue())
+				//add friend to list of online friends.
+				onlineFriends.add(friend);
+			
+		}
+		
+		return onlineFriends;
+
+	}
+	
+	/**
+	 * Returns a list of friends (User entities) that have not been active in the past 10 minutes or have hidden online activity.
+	 * @return List of CachedEntity representing Users that are offline.
+	 */
+	public List<CachedEntity> getOfflineFriends(){
+		
+		List<CachedEntity> offlineFriends = new ArrayList<CachedEntity>();
+		
+		for(CachedEntity friend : friendsOfUser){
+			Date lastMovement = (Date) friend.getProperty("locationEntryDatetime");
+			
+			boolean online = ((new Date().getTime() - lastMovement.getTime()) >= (10*60*1000));
+			Boolean hideActivity = (Boolean) friend.getProperty("hideUserActivity");
+			
+			//if the entry datetime hasn't occured in the past ten minutes or online activity is hidden..
+			if(lastMovement != null && (online || hideActivity.booleanValue()))
+				//add friend to list of offline friends.
+				offlineFriends.add(friend);			
+		}
+			
+		return offlineFriends;
+
+	}
+	
+	private List<CachedEntity> _getFriendsOfUser(){
 		//get a list of Friend entities who's userKey is equal to the user.
-		List<CachedEntity> friends = db.getFilteredList("Friend","userKey", user.getKey());
-		
+		List<CachedEntity> friends = db.getFilteredList("Friend","userKey", this.user.getKey());
+				
 		//From the list of friends, get all of their user keys.
 		List<Key> friendUserKeys = new ArrayList<Key>();
 		for(CachedEntity friend : friends){
-			
+				
 			Key friendKey = (Key) friend.getProperty("friendUserKey");
 			
 			friendUserKeys.add(friendKey);
 		}
 		
 		//return a list of User entities from the friend's user keys.
-		return db.getEntities(friendUserKeys);	
+		return db.getEntities(friendUserKeys);
 	}
 
 }
