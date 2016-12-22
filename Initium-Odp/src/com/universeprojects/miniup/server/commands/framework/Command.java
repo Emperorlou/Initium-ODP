73ac5395fcacf7a3b3d9a8a43a6353d805a7f04c
Andrew Carts <tacobowl8@gmail.com>
2016-12-21 22:13:42 -0500

    Moved tryParseStringToList method from CommandForgetAllCombatSites to Command.

--- Initium-Odp/src/com/universeprojects/miniup/server/commands/framework/Command.java
+++ Initium-Odp/src/com/universeprojects/miniup/server/commands/framework/Command.java
@@ -1,6 +1,8 @@
 package com.universeprojects.miniup.server.commands.framework;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -73,6 +75,29 @@ public abstract class Command extends OperationBase
 			return Long.parseLong(params.get(fieldName));
 		} catch (Exception _) {
 			throw new RuntimeException(this.getClass().getSimpleName()+" invalid call format, '"+fieldName+"' is not a valid id.");
+		}
+	}
+	
+	/** 
+	 * Tries to fetch fieldName from param list and throws if it cannot.
+	 * 
+	 * @param params
+	 * @param fieldName
+	 * @param delimitingCharacter
+	 * @return List of longs, parsed from param string
+	 */
+	protected List<Long> tryParseStringToList(Map<String,String> params, String fieldName, String delimitingCharacter)
+	{
+		try {
+			String unparsedString = params.get(fieldName);
+			String[] parsedArray = unparsedString.split(delimitingCharacter);
+			ArrayList<Long> parsedList = new ArrayList<Long>();
+			for(int i = 0; i < parsedArray.length; i++) {
+				parsedList.add(Long.parseLong(parsedArray[i]));
+			}
+			return parsedList;
+		} catch (Exception _) {
+			throw new RuntimeException(this.getClass().getSimpleName()+" invalid call format, '"+fieldName+"' is not a valid delimited list.");
 		}
 	}
 
