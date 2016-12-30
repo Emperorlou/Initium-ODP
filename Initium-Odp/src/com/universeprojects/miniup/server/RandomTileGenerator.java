326269fd53b469d5e8e3481e222e0a2fe5d948e8
bbeitman <bruce.beitman@gmail.com>
2016-12-30 15:28:02 -0500

    Adding RandomTileGenerator and tests for picking random grass tiles and z-index values.

---
+++ Initium-Odp/src/com/universeprojects/miniup/server/RandomTileGenerator.java
@@ -0,0 +1,56 @@
+package com.universeprojects.miniup.server;
+
+import java.util.Random;
+
+/**
+ * Generator to aid with 2D tile generation.
+ *
+ */
+public class RandomTileGenerator {
+
+    private RandomTileGenerator() {
+    }
+
+    //May look into refactoring this, it's ugly but gets the job done for now.
+    public static BuildingCell[][] getBuildingCells(int seed, int row, int col) {
+
+        BuildingCell[][] buildingCells = new BuildingCell[row][col];
+        for(int i = 0;i < row;i++) {
+            for(int j = 0;j < col;j++) {
+                //TODO: Turn this into a int and mod(%) it down, will need to take in number of possible tiles, and change BuildingCells filename to a index.
+                buildingCells[i][j] = new BuildingCell(
+                    "tile-grass" + (new Random(seed * (i*j+ i*10 + j)).nextInt(7)) + ".png", 
+                    (new Random(seed * (i*10 + j)).nextInt(10))
+                );
+            }
+        }
+        return buildingCells;
+    }
+}
+
+class BuildingCell {
+    String fileName;
+    int zIndex;
+    
+    public BuildingCell(String fileName, int zIndex) {
+        this.fileName = fileName;
+        this.zIndex = zIndex;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        
+        if (obj == null) {
+            return false;
+        }
+        if (obj == this) {
+            return true;
+        }
+        if (obj.getClass() != getClass()) {
+            return false;
+        }
+        
+        BuildingCell rhs = (BuildingCell) obj;
+        return rhs.fileName.equals(this.fileName) && rhs.zIndex == this.zIndex;
+    }
+}
