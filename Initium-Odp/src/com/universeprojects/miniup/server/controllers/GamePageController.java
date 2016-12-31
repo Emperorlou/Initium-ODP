d691dd0e08ab13b8ad42af0764097a6d47e7b907
DylanHolmes <holmes.dylan@hotmail.com>
2016-12-31 14:00:05 -0800

    - Added Game.jsp to odppages - Added GamePageController.java - Added GamePageUpdateService.java

---
+++ Initium-Odp/src/com/universeprojects/miniup/server/controllers/GamePageController.java
@@ -0,0 +1,37 @@
+package com.universeprojects.miniup.server.controllers;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.datastore.Key;
+import com.universeprojects.cacheddatastore.CachedEntity;
+import com.universeprojects.miniup.server.ODPDBAccess;
+import com.universeprojects.miniup.server.services.CombatService;
+import com.universeprojects.miniup.server.services.GamePageUpdateService;
+import com.universeprojects.web.PageController;
+
+public class GamePageController extends PageController {
+
+	protected GamePageController() {
+		super("game");
+	}
+
+	@Override
+	protected String processRequest(HttpServletRequest request,
+			HttpServletResponse arg1) throws ServletException, IOException {
+		
+		ODPDBAccess db = new ODPDBAccess(request);
+		CachedEntity location = db.getEntity((Key)db.getCurrentCharacter().getProperty("locationKey"));
+		CombatService combatService = new CombatService(db);
+
+		GamePageUpdateService updateService = new GamePageUpdateService(db, db.getCurrentUser(), db.getCurrentCharacter(), location, null);
+		
+		request.setAttribute("bannerTextOverlay", updateService.updateInBannerOverlayLinks());
+		request.setAttribute("inBannerCharacterWidget", updateService.updateInBannerCharacterWidget());
+		
+		return "/WEB-INF/odppages/game.jsp";
+	}
+}
