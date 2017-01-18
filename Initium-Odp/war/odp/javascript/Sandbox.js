29867cff9c3360a685c5f0827d7268bf506cca22
bbeitman <bruce.beitman@gmail.com>
2017-01-18 16:00:26 -0500

    Updating with "px" after updating locations

--- Initium-Odp/war/odp/javascript/Sandbox.js
+++ Initium-Odp/war/odp/javascript/Sandbox.js
@@ -452,8 +452,8 @@ function centerCellOnScreen(xCoord, yCoord) {
     yView = grid.offsetTop + yGrid;
     xGridOrigin = xView - viewportContainer.offsetWidth/2;
     yGridOrigin = yView - viewportContainer.offsetHeight/2;
-    grid.style.left = grid.offsetLeft - xGridOrigin;
-    grid.style.top = grid.offsetTop - yGridOrigin;
+    grid.style.left = (grid.offsetLeft - xGridOrigin) + "px" ;
+    grid.style.top = (grid.offsetTop - yGridOrigin) + "px";
 }
 function centerGridOnScreen() {
     actualGridWidth = (gridCellWidth + (gridCellWidth * gridTileWidth));
@@ -468,8 +468,8 @@ function centerGridOnScreen() {
     scaleTiles(true);
     scaledGridWidth = (gridCellWidth * scale  + (gridCellWidth * scale * gridTileWidth));
     scaledGridHeight = (gridCellHeight * scale + (gridCellHeight * scale * gridTileHeight));
-    grid.style.left = (viewport.offsetWidth - scaledGridWidth)/2;
-    grid.style.top = (viewport.offsetHeight - scaledGridHeight)/2;
+    grid.style.left = ((viewport.offsetWidth - scaledGridWidth)/2) + "px";
+    grid.style.top = ((viewport.offsetHeight - scaledGridHeight)/2) + "px";
 }
 function keyUnpress() {
     if (!e) {
