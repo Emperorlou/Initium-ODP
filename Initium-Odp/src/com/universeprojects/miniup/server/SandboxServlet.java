a696dbcea2c735d189ae9e1fd6bdb00cc17a0f0e
bbeitman <bruce.beitman@gmail.com>
2017-01-27 10:50:15 -0500

    Commenting out SandboxServlet

--- Initium-Odp/src/com/universeprojects/miniup/server/SandboxServlet.java
+++ Initium-Odp/src/com/universeprojects/miniup/server/SandboxServlet.java
@@ -13,22 +13,22 @@ import org.json.simple.JSONObject;
 
 public class SandboxServlet extends HttpServlet 
 {
-	private static final long serialVersionUID = 9209182266946047848L;
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
-	{
-		int width = Integer.parseInt(request.getParameter("width"));
-		int height = Integer.parseInt(request.getParameter("height"));
-		int seed = Integer.parseInt(request.getParameter("seed"));
-		int forestry = Integer.parseInt(request.getParameter("forestry"));
-		
-		response.setContentType("application/json");
-		response.setCharacterEncoding("UTF-8");
-		JSONObject result = new JSONObject();
-		GridMap gridMap = RandomTileGenerator.buildNewGrid(seed, width, height, forestry);
-		result.put("backgroundTiles", gridMap.getMap());
-		result.put("objectMap", gridMap.getGridObjects());
-
-		response.getWriter().write(result.toJSONString());
-	}
+//	private static final long serialVersionUID = 9209182266946047848L;
+//	
+//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+//	{
+//		int width = Integer.parseInt(request.getParameter("width"));
+//		int height = Integer.parseInt(request.getParameter("height"));
+//		int seed = Integer.parseInt(request.getParameter("seed"));
+//		int forestry = Integer.parseInt(request.getParameter("forestry"));
+//		
+//		response.setContentType("application/json");
+//		response.setCharacterEncoding("UTF-8");
+//		JSONObject result = new JSONObject();
+//		GridMap gridMap = RandomTileGenerator.buildNewGrid(seed, width, height, forestry);
+//		result.put("backgroundTiles", gridMap.getMap());
+//		result.put("objectMap", gridMap.getGridObjects());
+//
+//		response.getWriter().write(result.toJSONString());
+//	}
 }
