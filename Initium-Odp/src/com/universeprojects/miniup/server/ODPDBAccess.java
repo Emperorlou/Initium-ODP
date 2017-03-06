acc81f80de92d6087a1be88243bc677bce892e9f
Andrew Carts <tacobowl8@gmail.com>
2017-03-06 17:52:36 -0500

    Updates not equipable error message with a verb to make to grammatically correct.

--- Initium-Odp/src/com/universeprojects/miniup/server/ODPDBAccess.java
+++ Initium-Odp/src/com/universeprojects/miniup/server/ODPDBAccess.java
@@ -1277,7 +1277,7 @@ public class ODPDBAccess
 		String equipSlotRaw = (String)equipment.getProperty("equipSlot");
 		
 		if (equipSlotRaw==null)
-			throw new UserErrorMessage("This item not equipable.");
+			throw new UserErrorMessage("This item is not equipable.");
 		
 		if (equipSlotRaw.equals("Ring"))
 			equipSlotRaw = "LeftRing, RightRing";
