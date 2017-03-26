f9d7d23b78ea12b0ee904679113bb2492e770301
Marshall Warners <papa.initium@gmail.com>
2017-03-26 13:39:30 -0400

    Add autofix command for characters stuck in a location with no discoverable paths out

---
+++ Initium-Odp/src/com/universeprojects/miniup/server/commands/CommandCharacterAutofixStuckInLocation.java
@@ -0,0 +1,68 @@
+package com.universeprojects.miniup.server.commands;
+
+import java.util.Map;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.datastore.Key;
+import com.universeprojects.cacheddatastore.CachedDatastoreService;
+import com.universeprojects.cacheddatastore.CachedEntity;
+import com.universeprojects.miniup.CommonChecks;
+import com.universeprojects.miniup.server.GameUtils;
+import com.universeprojects.miniup.server.ODPDBAccess;
+import com.universeprojects.miniup.server.WebUtils;
+import com.universeprojects.miniup.server.commands.framework.Command;
+import com.universeprojects.miniup.server.commands.framework.UserErrorMessage;
+
+/**
+ * Checks to see if a character is stuck in a location with no discoverable paths available. If the character
+ * is stuck, their locationKey is set to the parentLocationKey of their current location. 
+ * 
+ * @author papamarsh
+ * 
+ */
+public class CommandCharacterAutofixStuckInLocation extends Command {
+
+	public CommandCharacterAutofixStuckInLocation(ODPDBAccess db, HttpServletRequest request,
+			HttpServletResponse response) {
+		super(db, request, response);
+	}
+
+	@Override
+	public void run(Map<String, String> parameters) throws UserErrorMessage {
+		ODPDBAccess db = ODPDBAccess.getInstance(request);
+		CachedDatastoreService ds = getDS();
+		
+		//grab the current character, its location, and the location's parent
+		CachedEntity character = db.getCurrentCharacter();
+		CachedEntity currentLocation = db.getEntity((Key) character.getProperty("locationKey"));
+		CachedEntity parentLocation = db.getParentLocation(ds, currentLocation);
+		
+		//special case for if user is in hell
+		if(currentLocation==db.getLocationByName("Ninth Circle of Hell"))
+		{
+			throw new UserErrorMessage("Nice Try! But there's no escape from Hell :)");
+		}
+		
+		//list the paths connected to the character's location
+		List<CachedEntity> pathList = db.getPathsByLocation(currentLocation.getKey());
+		for(CachedEntity checkPath:pathList)
+		{
+			CachedEntity checkPathLocation1 = db.getEntity((Key) checkPath.getProperty("location1Key"));
+			CachedEntity checkPathLocation2 = db.getEntity((Key) checkPath.getProperty("location1Key"));
+			Double checkPathDiscoveryChance = (Double) checkPath.getProperty("discoveryChance");
+			
+			//check if path is discoverable and both locationKeys are valid entities
+			if(checkPathLocation1!=null && checkPathLocation2!=null && checkPathDiscoveryChance>0)
+			{
+				throw new UserErrorMessage("At least one exit path exists");
+			}
+		}
+		//if no discoverable paths are found at the location, set character's locationKey to the parent
+		character.setProperty("locationKey", parentLocation);
+		//I think this should be a good enough fix, even if the parent is invalid since characters with
+		//an invalid locationKey are sent to their hometownKey (or default spawn loc if hometown is invalid)
+	}
+}
