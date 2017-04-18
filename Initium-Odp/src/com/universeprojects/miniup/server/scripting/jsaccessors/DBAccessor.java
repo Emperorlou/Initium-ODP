a415dde9d1c2914faebc4334adc2078b8b4afc26
spfiredrake <spfiredrake@gmail.com>
2017-04-18 13:50:01 -0400

    Remove beginBulkWriteMode in DBAccessor

--- Initium-Odp/src/com/universeprojects/miniup/server/scripting/jsaccessors/DBAccessor.java
+++ Initium-Odp/src/com/universeprojects/miniup/server/scripting/jsaccessors/DBAccessor.java
@@ -136,13 +136,11 @@ public class DBAccessor {
 	
 	public void moveItem(EntityWrapper currentCharacter, EntityWrapper item, EntityWrapper newContainer) throws UserErrorMessage 
 	{
-		db.getDB().beginBulkWriteMode();
 		db.doMoveItem(null, currentCharacter.wrappedEntity, item.wrappedEntity, newContainer.wrappedEntity);
 	}
 	
 	public boolean destroyItem(EntityWrapper item, EntityWrapper currentCharacter)
 	{
-		db.getDB().beginBulkWriteMode();
 		db.doDestroyEquipment(null, currentCharacter.wrappedEntity, item.wrappedEntity);
 		return true;
 	}
