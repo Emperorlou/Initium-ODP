0789d75e8480c359a0e5c29e723fa2533a4a968d
spfiredrake <spfiredrake@gmail.com>
2017-08-04 15:23:18 -0400

    Show load on leave and forget.

--- Initium-Odp/war/odp/javascript/script.js
+++ Initium-Odp/war/odp/javascript/script.js
@@ -1861,7 +1861,8 @@ function doDrinkBeer(eventObject)
 
 function doLeaveAndForgetCombatSite(eventObject, pathId)
 {
-	doCommand(eventObject, "LeaveAndForgetCombatSite", {"pathId" : pathId});
+	showBannerLoadingIcon();
+	doCommand(eventObject, "LeaveAndForgetCombatSite", {"pathId" : pathId}, clearPopupPermanentOverlay);
 }
 
 /**
