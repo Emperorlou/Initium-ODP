f3cbc1502de8024f5ec9c2e807fecc7fe868eaeb
Oskar Stangenberg <Crokoking@googlemail.com>
2018-01-31 21:57:30 +0100

    Added git clone batch files.

---
+++ clone-core.bat
@@ -0,0 +1,6 @@
+git clone https://bitbucket.org/universeprojects/gefcommon.git GEFCommon
+git clone https://github.com/Emperorlou/Cached-Datastore.git CachedDatastore
+git clone https://bitbucket.org/universeprojects/gameelementsframework.git GameElementsFramework
+git clone https://bitbucket.org/universeprojects/randomgenerators.git RandomGenerators
+git clone https://bitbucket.org/universeprojects/restclient.git RestClient
+git clone https://xp-dev.com/git/initium-core initium-core
---
+++ clone-odp.bat
@@ -0,0 +1,2 @@
+git clone https://bitbucket.org/universeprojects/gefcommon.git GEFCommon
+git clone https://github.com/Emperorlou/Cached-Datastore.git CachedDatastore
