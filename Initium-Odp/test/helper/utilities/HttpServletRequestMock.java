4a965b86171106c6306eee0593c2011e8099674b
Emperorlou <nikolasarmstrong@gmail.com>
2018-02-01 12:10:26 -0500

    Fixed request mock to implement missing methods.

--- Initium-Odp/test/helper/utilities/HttpServletRequestMock.java
+++ Initium-Odp/test/helper/utilities/HttpServletRequestMock.java
@@ -4,15 +4,24 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.security.Principal;
+import java.util.Collection;
 import java.util.Enumeration;
 import java.util.Locale;
 import java.util.Map;
 
+import javax.servlet.AsyncContext;
+import javax.servlet.DispatcherType;
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import javax.servlet.ServletInputStream;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.servlet.http.Part;
 
 public class HttpServletRequestMock implements HttpServletRequest {
 
@@ -339,6 +348,79 @@ public class HttpServletRequestMock implements HttpServletRequest {
 	public boolean isUserInRole(String arg0) {
 		
 		return false;
+	}
+
+	@Override
+	public ServletContext getServletContext() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public AsyncContext startAsync() throws IllegalStateException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public AsyncContext startAsync(ServletRequest servletRequest, ServletResponse servletResponse)
+			throws IllegalStateException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isAsyncStarted() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isAsyncSupported() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public AsyncContext getAsyncContext() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DispatcherType getDispatcherType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean authenticate(HttpServletResponse response) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void login(String username, String password) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void logout() throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Collection<Part> getParts() throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Part getPart(String name) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
