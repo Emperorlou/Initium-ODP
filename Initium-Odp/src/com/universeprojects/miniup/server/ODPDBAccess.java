0d52e4afe45a72777164d25768ea3862bb5f8d58
spfiredrake <spfiredrake@gmail.com>
2018-08-09 10:17:43 -0400

    Fix char key parsing for HCM item classification

--- Initium-Odp/src/com/universeprojects/miniup/server/ODPDBAccess.java
+++ Initium-Odp/src/com/universeprojects/miniup/server/ODPDBAccess.java
@@ -4961,7 +4961,7 @@ public class ODPDBAccess
         		
         		for(Entry<String,String> entry:damageMap.entrySet())
         		{
-        			damageKeys.add(KeyFactory.stringToKey(entry.getKey()));
+        			damageKeys.add(GameUtils.parseKey((entry.getKey()));
         			Double curDamage = Double.valueOf(entry.getValue());
         			characterMap.put(entry.getKey(), curDamage);
         		}
