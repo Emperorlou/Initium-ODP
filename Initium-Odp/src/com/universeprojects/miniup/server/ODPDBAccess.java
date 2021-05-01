ba6cb731ba051ee2b975ad7b584680076cecb041
Evanosity <iamevanosity@gmail.com>
2021-05-01 10:46:37 -0400

    Add reachableHtml to the ScriptType enum. I thought I did this; must have forgotten to commit ODPDBAccess.

--- Initium-Odp/src/com/universeprojects/miniup/server/ODPDBAccess.java
+++ Initium-Odp/src/com/universeprojects/miniup/server/ODPDBAccess.java
@@ -128,7 +128,7 @@ public class ODPDBAccess
 	{
 		directItem, directLocation, onAttack, onAttackHit, onDefend, onDefendHit, 
 		onMoveBegin, onMoveEnd, onServerTick, onCombatTick, combatItem, global,
-		ownerHtml;
+		ownerHtml, reachableHtml;
 	}
 
 	public enum StaticBuffables
